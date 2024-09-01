2b089d4f2a5ec43098368af653a88b0ed794fca1
amaotone <amane.suzu@gmail.com>
2024-09-01 21:24:17 +0900

    feat(bingo_board.dart): refactor BingoBoard to support multiple boards and add functionality to create new boards refactor(bingo_board.dart): update loading and saving logic to handle multiple boards delete(widget_test.dart): remove outdated widget test file as it is no longer relevant to the current implementation

--- lib/widgets/bingo_board.dart
+++ lib/widgets/bingo_board.dart
@@ -11,88 +11,76 @@ class BingoBoard extends StatefulWidget {
 }
 
 class _BingoBoardState extends State<BingoBoard> {
-  List<List<BingoCell>> _board = [];
+  List<List<List<BingoCell>>> _boards = [];
+  int _currentBoardIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    _loadBoard();
+    _loadBoards();
   }
 
-  Future<void> _loadBoard() async {
+  Future<void> _loadBoards() async {
     final prefs = await SharedPreferences.getInstance();
-    final String? boardJson = prefs.getString('bingo_board');
-    if (boardJson != null) {
-      final List<dynamic> decodedBoard = json.decode(boardJson);
+    final String? boardsJson = prefs.getString('bingo_boards');
+    if (boardsJson != null) {
+      final List<dynamic> decodedBoards = json.decode(boardsJson);
       setState(() {
-        _board = decodedBoard.map((row) {
-          return (row as List<dynamic>)
-              .map((cell) => BingoCell.fromJson(cell as Map<String, dynamic>))
-              .toList();
+        _boards = decodedBoards.map((board) {
+          return (board as List<dynamic>).map((row) {
+            return (row as List<dynamic>)
+                .map((cell) => BingoCell.fromJson(cell as Map<String, dynamic>))
+                .toList();
+          }).toList();
         }).toList();
       });
     } else {
       setState(() {
-        _board = List.generate(
-            5,
-            (_) =>
-                List.generate(5, (_) => BingoCell(goal: '', isChecked: false)));
+        _boards = [_createEmptyBoard()];
       });
     }
   }
 
-  Future<void> _saveBoard() async {
-    final prefs = await SharedPreferences.getInstance();
-    final String boardJson = json.encode(_board
-        .map((row) => row
-            .map((cell) => {'goal': cell.goal, 'isChecked': cell.isChecked})
-            .toList())
-        .toList());
-    await prefs.setString('bingo_board', boardJson);
+  List<List<BingoCell>> _createEmptyBoard() {
+    return List.generate(5,
+        (_) => List.generate(5, (_) => BingoCell(goal: '', isChecked: false)));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    if (_board.isEmpty) {
-      return const Center(child: CircularProgressIndicator());
-    }
+  Future<void> _saveBoards() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String boardsJson = json.encode(_boards
+        .map((board) => board
+            .map((row) => row
+                .map((cell) => {'goal': cell.goal, 'isChecked': cell.isChecked})
+                .toList())
+            .toList())
+        .toList());
+    await prefs.setString('bingo_boards', boardsJson);
+  }
 
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 5,
-        childAspectRatio: 1.0,
-        crossAxisSpacing: 2,
-        mainAxisSpacing: 2,
-      ),
-      itemCount: 25,
-      itemBuilder: (context, index) {
-        final row = index ~/ 5;
-        final col = index % 5;
-        return BingoCellWidget(
-          cell: _board[row][col],
-          onTap: () => _board[row][col].goal.isEmpty
-              ? _editCell(row, col)
-              : _toggleCheck(row, col),
-          onLongPress: () => _editCell(row, col),
-        );
-      },
-    );
+  void _addNewBoard() {
+    setState(() {
+      _boards.add(_createEmptyBoard());
+      _currentBoardIndex = _boards.length - 1;
+      _saveBoards();
+    });
   }
 
   void _toggleCheck(int row, int col) {
     setState(() {
-      _board[row][col].isChecked = !_board[row][col].isChecked;
-      _saveBoard();
+      _boards[_currentBoardIndex][row][col].isChecked =
+          !_boards[_currentBoardIndex][row][col].isChecked;
+      _saveBoards();
     });
   }
 
   void _editCell(int row, int col) {
     final TextEditingController controller =
-        TextEditingController(text: _board[row][col].goal);
+        TextEditingController(text: _boards[_currentBoardIndex][row][col].goal);
     showDialog(
       context: context,
       builder: (context) {
-        String newGoal = _board[row][col].goal;
+        String newGoal = _boards[_currentBoardIndex][row][col].goal;
         return AlertDialog(
           title: const Text('目標を入力'),
           content: TextField(
@@ -111,8 +99,8 @@ class _BingoBoardState extends State<BingoBoard> {
               child: const Text('保存'),
               onPressed: () {
                 setState(() {
-                  _board[row][col].goal = newGoal;
-                  _saveBoard();
+                  _boards[_currentBoardIndex][row][col].goal = newGoal;
+                  _saveBoards();
                 });
                 Navigator.of(context).pop();
               },
@@ -120,6 +108,79 @@ class _BingoBoardState extends State<BingoBoard> {
           ],
         );
       },
+    );
+  }
+
+  Widget _buildBingoGrid() {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+        childAspectRatio: 1.0,
+        crossAxisSpacing: 2,
+        mainAxisSpacing: 2,
+      ),
+      itemCount: 25,
+      itemBuilder: (context, index) {
+        final row = index ~/ 5;
+        final col = index % 5;
+        return BingoCellWidget(
+          cell: _boards[_currentBoardIndex][row][col],
+          onTap: () => _boards[_currentBoardIndex][row][col].goal.isEmpty
+              ? _editCell(row, col)
+              : _toggleCheck(row, col),
+          onLongPress: () => _editCell(row, col),
+        );
+      },
+    );
+  }
+
+  Widget _buildBoardList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: _boards.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('ビンゴカード ${index + 1}'),
+          selected: index == _currentBoardIndex,
+          onTap: () {
+            setState(() {
+              _currentBoardIndex = index;
+            });
+          },
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_boards.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Column(
+      children: [
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                _buildBingoGrid(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: _addNewBoard,
+                    child: const Text('新しいビンゴカードを追加'),
+                  ),
+                ),
+                _buildBoardList(),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
